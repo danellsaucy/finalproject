416fc3b7578aca267ab8158b2380efeced1c6617
Danell F <danellf@uw.edu>
2020-02-29 12:06:45 -0800

    Finshed editing the functions for the tables

--- scripts/sum_table.R
+++ scripts/sum_table.R
@@ -1,17 +1,34 @@
 library(dplyr)
 library(stringr)
+library(lintr)
+library(styler)
 
-test <- read.csv("./data/Google-Playstore-Full.csv", stringsAsFactors = FALSE)
-test <- test %>% filter(!is.na(as.numeric(Rating)))
-test <- test %>% filter(!is.na(Rating))
-test <- test %>% filter(!is.na(as.numeric(Reviews)))
-test <- test %>% filter(!is.na(Reviews))
-temp <- test %>% group_by(Category) %>% summarise(avg_rating = mean(as.numeric(Rating), na.rm = TRUE), avg_reviews = mean(as.numeric(Reviews), na.rm = TRUE), avg_installs = mean(as.numeric(gsub("[+]", "", str_replace_all(Installs, "[[:punct:]]", "")))), count = length(Category))
+get_summary_table_google <- function(google_df) {
+  google_df <- google_df %>% filter(!is.na(as.numeric(Rating)))
+  google_df <- google_df %>% filter(!is.na(Rating))
+  google_df <- google_df %>% filter(!is.na(as.numeric(Reviews)))
+  google_df <- google_df %>% filter(!is.na(Reviews))
+  ret_table <- google_df %>%
+    group_by(Category) %>%
+    summarise(
+      avg_rating = mean(as.numeric(Rating), na.rm = TRUE),
+      avg_price = mean(as.numeric(gsub("[$]", "", Price)),
+        na.rm = TRUE
+      ),
+      count = length(Category)
+    ) %>%
+    arrange(desc(count))
+  return(ret_table)
+}
 
-get_summary_table_google <- function(dataset) {
-  dataset <- dataset %>% filter(!is.na(as.numeric(Rating)))
-  dataset <- dataset %>% filter(!is.na(Rating))
-  dataset %>% group_by(Category) %>% summarise(avg_rating = mean(as.numeric(Rating), na.rm = TRUE))
-  ret_table <- 
-  return (ret)
-} 
+get_summary_table_apple <- function(apple_df) {
+  ret_table2 <- apple_df %>%
+    group_by(prime_genre) %>%
+    summarise(
+      avg_rating = mean(as.numeric(user_rating), na.rm = TRUE),
+      avg_price = mean(price, na.rm = TRUE),
+      count = length(prime_genre)
+    ) %>%
+    arrange(desc(count))
+  return(ret_table)
+}
