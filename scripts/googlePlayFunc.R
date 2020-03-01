c164e126725ac1e87607ab7fa21213c405b29c0f
Edward Sung <edwardsung63@gmail.com>
2020-02-29 17:02:02 -0800

    added a sum infor function for google play

---
+++ scripts/googlePlayFunc.R
@@ -0,0 +1,13 @@
+# For Google Store
+# A function that takes in a dataset and returns a list of info about it:
+get_summary_info <- function(dataset) {
+  ret <- list()
+  ret$length <- length(dataset)
+  # do some more interesting stuff
+  ret$total_earned <- sum(dataset$price)
+  ret$avg_reviews <- mean(as.numeric(dataset$Reviews), na.rm = FALSE)
+  ret$uniq_cont_rating <- unique(dataset$Content.Rating)
+  return (ret)
+} 
+
+get_summary_info(google_cleaned)
