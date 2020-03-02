39c878f76b3c4657a6f1e6542ef030021448f2ab
Danell F <danellf@uw.edu>
2020-03-01 22:44:49 -0800

    Completed and styled code all scripts, added two plot scripts and incorpated into the index file

---
+++ scripts/chart_two.R
@@ -0,0 +1,36 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(gapminder)
+library(ggpubr)
+library(plotly)
+
+source("./scripts/sum_table.R")
+
+plot_two_google <- plot_ly(
+  test,
+  x = ~avg_rating, y = ~avg_price,
+  # Hover text:
+  text = ~ paste(
+    "Category: ", Category,
+    "<br>Average rating: ", avg_rating,
+    "<br>Average price:", avg_price,
+    "<br>Total number of applications: ", count
+  ),
+  color = ~avg_price, size = ~avg_price
+)
+plot_two_google <- plot_two_google %>% layout(title = "Google")
+
+plot_two_apple <- plot_ly(
+  test2,
+  x = ~avg_rating, y = ~avg_price,
+  # Hover text:
+  text = ~ paste(
+    "Category: ", prime_genre,
+    "<br>Average rating: ", avg_rating,
+    "<br>Average price:", avg_price,
+    "<br>Total number of applications: ", count
+  ),
+  color = ~avg_price, size = ~avg_price,
+)
+plot_two_apple <- plot_two_apple %>% layout(title = "Apple")
